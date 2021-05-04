d86a9965de9780225e95158e7daba366e1d7e365
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-04 15:41:35 +0200

    ~// Implementation of make_decision_split_node method

--- FML_Worksheet05/decision_tree.py
+++ FML_Worksheet05/decision_tree.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from base import Tree
+from base import (Node, Tree)
 
 
 def make_decision_split_node(node, feature_indices):
@@ -10,7 +10,52 @@ def make_decision_split_node(node, feature_indices):
     :param feature_indices:
     :return:
     """
-    pass
+    n, D = node.data.shape
+
+    # Find best feature j (among 'feature_indices') and best threshold t for the split
+    e_min = 1e100
+    j_min, t_min = 0, 0
+    for j in feature_indices:
+        # Remove duplicate features
+        dj = np.sort(np.unique(node.data[:, j]))
+        # Compute candidate thresholds
+        tj = (dj[1:] + dj[:-1]) / 2
+
+        # Compute Gini-impurity of resulting children nodes for each candidate threshold
+        for t in tj:
+            left_indices = node.data[:, j] <= t
+
+            nl = np.sum(node.data[:, j] <= t)
+            ll = node.labels[left_indices]
+            el = nl * (1 - np.sum(np.square(np.bincount(ll)/nl)))
+
+            nr = n - nl
+            lr = node.labels[~left_indices]
+            er = nr * (1 - np.sum(np.square(np.bincount(lr)/nr)))
+
+            if el + er < e_min:
+                e_min = el + er
+                j_min = j
+                t_min = t
+
+    # Create children
+    left = Node()
+    right = Node()
+
+    # Initialize 'left' and 'right' with the data subsets and labels
+    # according to the optimal split found above
+    left.data = node.data[node.data[:, j_min] <= t_min, :]
+    left.labels = node.labels[node.data[:, j_min] <= t_min]
+
+    right.data = node.data[node.data[:, j_min] > t_min, :]
+    right.labels = node.labels[node.data[:, j_min] > t_min]
+
+    node.left = left
+    node.right = right
+    node.feature = j_min
+    node.threshold = t_min
+
+    return left, right
 
 
 def make_decision_leaf_node(node):
