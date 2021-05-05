a9e0ef74e70c5534de046f8393846c1f3d110fcb
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-05 13:05:17 +0200

    ~// Stringdocs

--- FML_Worksheet05/density_forest.py
+++ FML_Worksheet05/density_forest.py
@@ -3,21 +3,31 @@ import numpy as np
 from density_tree import DensityTree
 
 
-class DensityForest():
+class DensityForest:
+    """
+    Density Forest:
     """
 
-    """
+    def __init__(self, n_trees=100):
+        """
 
-    def __init__(self, n_trees):
-        # create ensemble
+        :param n_trees: int, default=100
+            The number of trees in the forest.
+        """
         self.trees_ = [DensityTree() for i in range(n_trees)]
 
     def fit(self, data, prior, n_min=20):
         """
 
-        :param data:
-        :param prior:
-        :param n_min:
+        :param data: array-like of shape (n_instances, n_features)
+            Design matrix
+
+        :param prior: int
+            Prior probability of target class
+
+        :param n_min: int, default=20
+            The minimum number of samples required to split an internal node
+
         :return:
         """
         for tree in self.trees_:
@@ -26,8 +36,12 @@ class DensityForest():
 
     def predict(self, x):
         """
+        Compute mean mean of posterior probabilities of each tree in self.trees_
 
-        :param x:
-        :return:
+        :param x: array-like of shape (1, n_features)
+            Sample point (test instance)
+
+        :return: int
+            Return mean of posterior probabilities of each tree in self.trees_
         """
         return np.mean([tree.predict(x) for tree in self.trees_])
