c8d61b58fc2558b4f25446fbafb5d2fc60205aba
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-05 10:33:43 +0200

    ~// Density forest class

--- FML_Worksheet05/density_forest.py
+++ FML_Worksheet05/density_forest.py
@@ -1,16 +1,33 @@
 import numpy as np
 
+from density_tree import DensityTree
+
 
 class DensityForest():
+    """
+
+    """
+
     def __init__(self, n_trees):
         # create ensemble
-        self.trees = [DensityTree() for i in range(n_trees)]
+        self.trees_ = [DensityTree() for i in range(n_trees)]
 
-    def train(self, data, prior, n_min=20):
-        for tree in self.trees:
-            # train each tree, using a bootstrap sample of the data
-            ... # your code here
+    def fit(self, data, prior, n_min=20):
+        """
+
+        :param data:
+        :param prior:
+        :param n_min:
+        :return:
+        """
+        for tree in self.trees_:
+            bootstrap_data = data[np.random.choice(len(data), size=len(data))]
+            tree.fit(bootstrap_data, prior, n_min)
 
     def predict(self, x):
-        # compute the ensemble prediction
-        return ... # your code here
+        """
+
+        :param x:
+        :return:
+        """
+        return np.mean([tree.predict(x) for tree in self.trees_])
