e6cdb8da987dffbbb99870f5e2940b7c2c9ce9ee
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-08 23:15:47 +0200

    ~// make_regression_split_node method

--- FML_Worksheet06/regression_tree.py
+++ FML_Worksheet06/regression_tree.py
@@ -27,7 +27,53 @@ def make_regression_split_node(node, feature_indices):
     :param feature_indices:
     :return:
     """
-    pass
+    n, D = node.data.shape
+    # Find best feature j (among 'feature_indices') and best threshold t for the split
+    e_min = 1e100
+    j_min, t_min = 0, 0
+    for j in feature_indices:
+        # Duplicate feature values have to be removed because candidate thresholds are
+        # the midpoints of consecutive feature values, not the feature value itself
+        dj = np.sort(np.unique(node.data[:, j]))
+        # Compute candidate thresholds
+        tj = (dj[1:] + dj[:-1]) / 2
+
+        # Compute Gini-impurity of resulting children nodes for each candidate threshold
+        for t in tj:
+            left_indices = node.data[:, j] <= t
+            nl = np.sum(left_indices)
+            ll = node.labels[left_indices]
+            # el = nl * (1 - np.sum(np.square(np.bincount(ll) / nl)))
+            el = ((ll - ll.mean()) ** 2).sum()
+            nr = n - nl
+            # lr = node.labels[node.data[:, j] > t]
+            lr = node.labels[~left_indices]
+            # er = nr * (1 - np.sum(np.square(np.bincount(lr) / nr)))
+            er = ((lr - lr.mean())**2).sum()
+
+            if el + er < e_min:
+                e_min = el + er
+                j_min = j
+                t_min = t
+
+    # Create children
+    left = Node()
+    right = Node()
+
+    # Initialize 'left' and 'right' with the data subsets and labels
+    # according to the optimal split found above
+    left.data = node.data[node.data[:, j_min] <= t_min, :]
+    left.labels = node.labels[node.data[:, j_min] <= t_min]
+
+    right.data = node.data[node.data[:, j_min] > t_min, :]
+    right.labels = node.labels[node.data[:, j_min] > t_min]
+
+    node.left = left
+    node.right = right
+    node.feature = j_min
+    node.threshold = t_min
+
+    return left, right
 
 
 def make_regression_leaf_node(node):
