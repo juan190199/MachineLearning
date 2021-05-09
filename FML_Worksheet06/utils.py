24ace3d80099fc75a08edae08f81784131ce9944
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-10 00:31:31 +0200

    ~// Including test data_method to evaluate errors with shuffled data

--- FML_Worksheet06/utils.py
+++ FML_Worksheet06/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from sklearn import model_selection
 
+from data.my_dataset import MyDataset
+
 
 def mean_squared_error(prediction, target):
     """
@@ -15,7 +17,7 @@ def mean_squared_error(prediction, target):
     return np.sum(np.square(prediction - target)) / N
 
 
-def k_fold_cv(model, feats, targets, k):
+def k_fold_cv(model, feats, targets, k=10):
     """
 
     :param model:
@@ -32,3 +34,40 @@ def k_fold_cv(model, feats, targets, k):
         err_train.append(mean_squared_error(model.predict(feats[train]), targets[train]))
         err_test.append(mean_squared_error(model.predict(feats[test]), targets[test]))
     return np.mean(err_train), np.std(err_train), np.mean(err_test), np.std(err_test)
+
+
+def test_data(models, oerrors, data, n_shuffle_sets, pred_variable='rating'):
+    """
+
+    :param data:
+    :param n_shuffle_sets:
+    :param pred_variable:
+    :return:
+    """
+    shuffle_sets = []
+    # Random seed to make results reproducible
+    np.random.seed(1089045)
+    # Shuffling works in place, therefore copy is retrieved
+    rating = np.array(data[pred_variable]).copy()
+    for i in range(n_shuffle_sets):
+        np.random.shuffle(rating)
+        shuffled_data = data.copy()
+        shuffled_data[pred_variable] = rating
+        shuffle_sets.append(MyDataset(shuffled_data))
+
+    # Evaluate shuffle sets
+    oerrors = np.array([oerrors]).T
+    shuffled_errors = np.empty((len(models), n_shuffle_sets))
+    for i, data_set in enumerate(shuffle_sets):
+        print("Run shuffle set no. %i" % (i + 1))
+        feats = data_set.get_X_oc_()
+        targets = data_set.targets
+        assert feats.shape[0] == targets.shape[0]
+        for j, model in enumerate(models):
+            err_train, std_err_train, err_test, std_err_test = k_fold_cv(model, feats, targets)
+            shuffled_errors[j, i] = err_test
+
+    print(shuffled_errors)
+    n_greater_errors_models = np.sum((shuffled_errors > oerrors), axis=1)
+
+    return n_greater_errors_models
