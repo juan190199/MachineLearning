4efcd736ae53886c1aaf4a3e188fc3f62bdd1a87
juan190199 <juanmanueldasilvatome@gmail.com>
2021-05-23 13:26:36 +0200

    ~// Reformat

--- FML_Worksheet08/utils.py
+++ FML_Worksheet08/utils.py
@@ -12,21 +12,21 @@ def orthogonal_matching_pursuit(X, y, iterations):
 
     """
     dim = X.shape[1]
+    theta_hat = np.zeros((dim, iterations))
     S = []
-    res = y
     feat_idx = list(range(X.shape[1]))
-    theta_hat = np.empty((dim, iterations))
+    res = y
     for it in range(iterations):
         # Correlations with the current residual
         cor = [np.abs(np.dot(X[:, j].T, res)) for j in feat_idx]
         # Find maximum
         j_max = np.argmax(np.array(cor))
         # Add most important feature to selected set S
-        S.append((feat_idx.pop(j_max)))
+        S.append(feat_idx.pop(j_max))
         # Update the residual with the projection by solving least squares problem
-        new_X = X[:, S]
-        theta = lstsq(new_X, y, rcond=None)
-        res = y - np.dot(new_X, theta[0])
+        X_c = X[:, S]
+        theta = lstsq(X_c, y, rcond=None)
+        res = y - np.dot(X_c, theta[0])
         theta_hat[S, it] = theta[0]
 
     return theta_hat
