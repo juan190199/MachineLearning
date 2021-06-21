93cc6b8f0d79a65634ad09fff46f4e34e08d5478
juan190199 <juanmanueldasilvatome@gmail.com>
2021-06-21 23:41:43 +0100

    ~// KRR

---
+++ FML_09/kernel_ridge.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# https://github.com/supenova1604/kernel_ridge_regr/blob/master/kerner_ridge_regr.py
+# https://github.com/ptocca/KRRPM/blob/main/src/krrpm/krrpm.py
+
+class KernelRidge():
+    """"""
+
+
