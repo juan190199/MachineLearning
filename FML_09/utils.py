b70ad5b17ddbede70aabe92e989b567c0e2d9d42
juan190199 <juanmanueldasilvatome@gmail.com>
2021-07-01 02:26:16 +0100

    ~// Calculate circle from points

--- FML_09/utils.py
+++ FML_09/utils.py
@@ -1,7 +1,9 @@
+import numpy as np
 
 
 def to_img(img, X_new, y_new):
     """
+    Complete image with predicted pixels y_test for X_test data
 
     :param img:
     :param y_new:
@@ -11,3 +13,40 @@ def to_img(img, X_new, y_new):
     img_regressed[X_new[:, 0], X_new[:, 1]] = y_new
 
     return img_regressed
+
+
+def circle_from_points(points):
+    """
+
+    :param points:
+    :return:
+    """
+    offset = points[0]
+    p = points - offset
+
+    # 2x2 linear equations
+    A = 2 * p[1:, :]
+    b = p[1:, 0]**2 + p[1:, 1]**2
+
+    try:
+        c = np.dot(np.linalg.inv(A), b)
+        r = np.sqrt(c[0]**2 + c[1]**2)
+    except np.linalg.linalg.LinAlgError:
+        # If the points are on a straight line, there is no solution
+        return np.NaN * np.zeros(2), np.NaN
+    else:
+        return c + offset, r
+
+
+def ransac_iter(data):
+    """
+    Perform one RANSAC iteration
+
+    :param data:
+    :return:
+    """
+    # Choose 3 points
+    # Calculate circle with the given points
+    # Check if points agree
+    # Compare with threshold
+
