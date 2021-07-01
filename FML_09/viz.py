589b47ef29b9d881ff48a40133c30169c8d8ef23
juan190199 <juanmanueldasilvatome@gmail.com>
2021-07-01 18:00:21 +0100

    ~// Plot circles and data

--- FML_09/viz.py
+++ FML_09/viz.py
@@ -1,4 +1,23 @@
 import matplotlib.pyplot as plt
 
-def plot_recovered_image():
-    ...
+
+def plot_circles_and_data(model, outliers):
+    """
+
+    :param model:
+    :param outliers:
+    :return:
+    """
+    plt.figure(figsize=(10, 10))
+    for circ in model:
+        plt.scatter(circ['MCS'][:, 0], circ['MCS'][:, 1], c='green')
+        plt.gca().add_patch(plt.Circle(circ['c'], radius=circ['r'], fill=False))
+
+    plt.scatter(outliers[:, 0], outliers[:, 1], c='red')
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.tight_layout()
+
