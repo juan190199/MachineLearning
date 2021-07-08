cff8328adf90bd8eb02576229df1b73c4977b9f4
juan190199 <juanmanueldasilvatome@gmail.com>
2021-07-08 22:43:51 +0100

    ~// Viz data

---
+++ FML_Extras/principal_component_analysis/viz.py
@@ -0,0 +1,20 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+
+
+def plot_figs(fig_num, elev, azim, coord_dens):
+    fig = plt.figure(fig_num, figsize=(4, 3))
+    plt.clf()
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=elev, azim=azim)
+    a, b, c, density = coord_dens
+    ax.scatter(a[::10], b[::10], c[::10], c=density[::10], marker='+', alpha=.4)
+    Y = np.c_[a, b, c]
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+
+    plt.show()
+
