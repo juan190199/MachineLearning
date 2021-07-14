01a429f84104eb3662d5e631872ca2ba308ec204
juan190199 <juanmanueldasilvatome@gmail.com>
2021-07-14 23:49:32 +0100

    ~// Highdimensional pca

--- FML_Extras/principal_component_analysis/pca.py
+++ FML_Extras/principal_component_analysis/pca.py
@@ -18,9 +18,33 @@ def pca(X, n_dim):
     cov = np.dot(X.T, X)
 
     eigvals, eigvecs = np.linalg.eig(cov)
-    idx_ = np.argsort(eigvals)[:n_dim]
-    principal_eigvalues = eigvals[idx_]
-    principal_eigvecs = eigvecs[:, idx_]
+    idxs_ = np.argsort(-eigvals)[:n_dim]
+    principal_eigvalues = eigvals[idxs_]
+    principal_eigvecs = eigvecs[:, idxs_]
+
+    ndim_X = np.dot(X, principal_eigvecs)
+    return ndim_X
+
+
+def highdim_pca(X, n_dim):
+    """
+
+    :param X:
+    :param n_features:
+    :return:
+    """
+    n = X.shape[0]
+    X = X - np.mean(X, axis=0, keepdims=True)
+
+    ncov = np.dot(X, X.T)
+
+    neigvals, neigvecs = np.linalg.eig(ncov)
+    idxs_ = np.argsort(-neigvals)[:n_dim]
+    principal_neigvals = neigvals[idxs_]
+    principal_neigvecs = neigvecs[:, idxs_]
+
+    principal_eigvecs = np.dot(X.T, principal_neigvecs)
+    principal_eigvecs = principal_eigvecs / (n * principal_neigvals.reshape(-1, n_dim)) ** 0.5
 
     ndim_X = np.dot(X, principal_eigvecs)
     return ndim_X
